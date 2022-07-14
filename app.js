48f700cb546019a3d8c429f4f0f52ae76801fca5
Kevin Leister <kevinleister07@gmail.com>
2022-07-14 00:13:14 -0500

    set up sign up page

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+console.log(firstName, lastName, email)
+});
 
 app.listen(3000, function(){
 console.log("Server is running on port 3000.");
 });
+
+// mailchimp api key
+// 2060e58e514ea79e073c97a31c53f836-us11
